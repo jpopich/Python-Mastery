51e40ad06cdc199845b30140116f9e5e370c9e2c
jpopich <jpopich@uno.edu>
2025-12-11 17:40:07 -0600

    Finished Integers in Chapter 2

---
+++ 1. Python Crash Course/Part 1 - Basics/Chapter-02-Variables and Simple Data Types/numbers.py
@@ -0,0 +1,53 @@
+# TRY IT YOURSELF
+# 2-10 Adding Comments:
+# We've added comments to a lot of things. This TIY can be skipped........
+# 2-11 enter import this into a python terminal COMPLETE
+
+print('Basic Math')
+print(2+3)
+print(3-2)
+print(2*3)
+print(3/2)
+
+print("\n")
+print('Same as math.pow')
+print(3**2)
+print(3**3)
+print(10**6)
+
+print("\n")
+print('Order of Operations:')
+print(2+3*4)
+print((2+3) * 4)
+
+print("\n")
+print('Floats')
+print(0.1 + 0.1)
+print(0.2 + 0.2)
+print(2 * 0.1)
+print(2 * 0.2)
+
+print("\n")
+print('Arbitrary amount of zeroes in decimal:')
+print(0.2 + 0.1)
+print(3 * 0.1)
+
+print("\n")
+print("Avoiding type error with str() Function")
+age = 23
+message = "Happy " + str(age) + "rd Birthday!"
+print(message)
+
+#TRY IT YOURSELF
+# 2-8 Number Eight:
+print(5+3)
+print(9-1)
+print(4*2)
+print(16/2)
+print("\n")
+print("\n")
+
+
+# 2-9 Favorite Number
+favNum = 13
+print("My favorite number is: " + str(favNum))
